b68a881f56bc471eeaf7430c9ea007514f577944
GustavolLo <goodluke111931@gmail.com>
2020-10-09 23:41:27 +0200

    Getting books from a database sql server.

--- app.js
+++ app.js
@@ -3,9 +3,23 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const sql = require('mssql');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const confg = {
+  user: 'express-library',
+  password: '34AVb4R5*a',
+  server: 'express-library.database.windows.net', // You can use 'localhost\\instance' to connect to named instance
+  database: 'ExpressLibrary',
+
+  options: {
+    encrypt: true // use this if you are on Windows Azure
+  }
+};
+
+sql.connect(confg).catch((error) => debug(error));
 
 app.use(morgan('tiny'));
 
