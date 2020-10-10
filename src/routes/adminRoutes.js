4f3060acdfba8329002d4e42e711dda3e5d71740
GustavolLo <goodluke111931@gmail.com>
2020-10-10 12:24:53 +0200

    Adding admin router.

---
+++ src/routes/adminRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const adminRouter = express.Router();
+
+function router() {
+  adminRouter.route('/')
+    .get((req, res) => {
+      res.send('Inserting books.');
+    });
+  return adminRouter;
+}
+
+module.exports = router;
