fcb7cb1f2d60c103c8515b2f0660cedbbf01bef4
GustavolLo <goodluke111931@gmail.com>
2020-10-10 11:16:44 +0200

    Adding middleware for specific book request.

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -21,21 +21,24 @@ function router(nav) {
     });
 
   bookRouter.route('/:id')
-    .get((req, res) => {
+    .all((req, res, next) => {
       (async function query() {
         const { id } = req.params;
         const request = new mssql.Request();
-
         const { recordset } = await request
           .input('id', mssql.Int, id)
           .query('select * from books where id = @id');
-        res.render('bookView',
-          {
-            title: 'Library',
-            nav,
-            book: recordset[0]
-          });
+        [req.book] = recordset;
+        next();
       }());
+    })
+    .get((req, res) => {
+      res.render('bookView',
+        {
+          title: 'Library',
+          nav,
+          book: req.book
+        });
     });
 
   return bookRouter;
