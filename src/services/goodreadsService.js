a7b4b8cbe67354e544fb3da9b56815af48ed9685
GustavolLo <goodluke111931@gmail.com>
2020-10-21 19:47:15 +0200

    Adding goodreads API calls.

--- src/services/goodreadsService.js
+++ src/services/goodreadsService.js
@@ -1,17 +1,18 @@
-const debug = require('debug')('app:goodreadsService');
 const axios = require('axios');
 const xml2js = require('xml2js');
+const debug = require('debug')('app:goodreadsService');
 
 const parser = xml2js.Parser({ explicitArray: false });
 
 function goodreadsService() {
-  function getBookById() {
+  function getBookById(id) {
     return new Promise((resolve, reject) => {
-      axios.get('https://www.goodreads.com/book/show/656.xml?key=55MUOVyvd5iVWQ43V9tPA')
+      axios.get(`https://www.goodreads.com/book/show/${id}.xml?key=55MUOVyvd5iVWQ43V9tPA`)
         .then((response) => {
           parser.parseString(response.data, (error, result) => {
             if (error) {
               debug(error);
+              reject(error);
             } else {
               debug(result);
               resolve(result.GoodreadsResponse.book);
